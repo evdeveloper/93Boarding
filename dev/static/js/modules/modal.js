7bb78bfc6bce13cb6d7a4a319b51033b3c25b558
«Evgeny» <mushnikovz@mail.ru>
2025-10-19 13:41:42 +0300

    Добавил страницу treds, внёс правки

--- dev/static/js/modules/modal.js
+++ dev/static/js/modules/modal.js
@@ -3,9 +3,9 @@ import { BODY } from "./../helpers/constants.js";
 export function openModal(id) {
   document.querySelectorAll('.modal').forEach(modal => modal.classList.remove('show'));
   document.querySelector(`[data-modal-target="${id}"]`).classList.add('show');
-  BODY.classList.add('hidden');
+  // BODY.classList.add('hidden');
 }
 export function closeModal(id) {
   document.querySelector(`[data-modal-target="${id}"]`).classList.remove('show');
-  BODY.classList.remove('hidden');
+  // BODY.classList.remove('hidden');
 }
